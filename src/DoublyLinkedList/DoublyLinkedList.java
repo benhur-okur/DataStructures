30d61500a9816382c370e6a1d5e5b8a914dec9df
benhur-okur <117393881+benhur-okur@users.noreply.github.com>
2023-08-19 00:40:20 +0300

    finished DoublyLinkedList
    finished but Interview Questions

--- src/DoublyLinkedList/DoublyLinkedList.java
+++ src/DoublyLinkedList/DoublyLinkedList.java
@@ -13,6 +13,119 @@ public class DoublyLinkedList {
             this.value = value;
         }
     }
+    public Node remove(int index) {
+        if (index < 0 || index >= length) {return null;}
+        if (index == 0) {
+            return removeFirst();
+        }
+        if (index == length-1) {
+            return removeLast();
+        }
+        Node removeIt = get(index);
+        Node before = get(index).prev;
+        Node after = get(index).next;
+
+        before.next = after;
+        after.prev = before;
+
+        removeIt.next = null;
+        removeIt.prev = null;
+
+        length--;
+        return removeIt;
+    }
+    public boolean insert(int index, int value) {
+        Node newNode = new Node(value);
+        Node before = head;
+        Node after = head;
+        if (index < 0 || index > length ) {return false;}
+        if (index == 0) {
+            prepend(value);
+            return true;
+        }
+        if (index == length) {
+            append(value);
+            return true;
+        }
+        before = get(index).prev;
+        after = get(index);
+
+        before.next = newNode;
+        after.prev = newNode;
+        newNode.prev = before;
+        newNode.next = after;
+
+        length++;
+        return true;
+
+    }
+    public boolean set (int index, int value) {
+        if(index < 0 || index >= length) {return false;}
+        Node temp = head;
+        temp = get(index);
+        temp.value = value;
+
+        return true;
+    }
+
+    public Node get(int index) {
+        if (index < 0 || index >= length) {return null;}
+        Node temp = head;
+        if (index < length/2) {
+            for (int i = 0;i<index;i++) {
+                temp = temp.next;
+            }
+            return temp;
+        } else {
+            temp = tail;
+            for (int i = 1;i<length-index;i++) {
+                temp = temp.prev;
+            }
+            return temp;
+        }
+    }
+
+    public Node removeFirst () {
+        Node firstNode = head;
+        if (head == null) {return null;}
+        if (head == tail) {
+            head = null;
+            tail = null;
+        } else {
+            head = head.next;
+            firstNode.next = null;
+            head.prev = null;
+        }
+        length--;
+        return firstNode;
+    }
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        length++;
+    }
+    public Node removeLast () {
+        if (tail == null) {return null;}
+        Node temp = tail;
+        if (length == 1) {
+            head = null;
+            tail = null;
+        } else {
+            tail = tail.prev;
+            temp.prev = null;
+            tail.next = null;
+        }
+        length--;
+        return temp;
+
+    }
 
     public void append(int value) {
         Node newNode = new Node(value);
--- src/DoublyLinkedList/Main.java
+++ src/DoublyLinkedList/Main.java
@@ -5,11 +5,19 @@ public class Main {
     public static void main(String[] args) {
         DoublyLinkedList myDLL = new DoublyLinkedList(2);
 
+
         myDLL.append(3);
+        myDLL.append(4);
+        myDLL.append(5);
+
+        myDLL.insert(1, 31);
+
+        myDLL.remove(0);
 
         myDLL.getHead();
         myDLL.getTail();
         myDLL.getLength();
+
 
         myDLL.printAll();
     }
